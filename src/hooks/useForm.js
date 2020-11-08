c439617e44228a70e72f5520c58ac6b99ccb4069
Angel Santillan <angelsb94@gmail.com>
2020-11-08 17:20:07 -0600

    custom hooks

---
+++ src/hooks/useForm.js
@@ -0,0 +1,19 @@
+import { useEffect, useState } from 'react'
+
+const useForm = (initialStatev = {}) => {
+
+  const [ values, setValues ] = useState(initialStatev);
+
+  const handleInpuntChange = ({target}) => {
+    setValues({
+      ...values,
+      [ target.name ] : target.value
+    });
+  }
+
+  return [ values, handleInpuntChange ]
+
+
+}
+
+export default useForm
--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import FormWithCustomHook from './components/02-useEffect/FormWithCustomHook';
 // import CounterApp from './components/01-useState/CounterApp';
 // import CounterWithCustinHook from './components/01-useState/CounterWithCustinHook';
-import SimpleForm from './components/02-useEffect/SimpleForm';
+// import SimpleForm from './components/02-useEffect/SimpleForm';
 // import HookApp from './HookApp';
 
 
 ReactDOM.render(
-    <SimpleForm />,
+    <FormWithCustomHook />,
   document.getElementById('root')
 );
