18b400fe2a0651f1e844de31488e33e18f514546
Angel Santillan <angelsb94@gmail.com>
2020-12-13 12:33:31 -0600

    concepto basico de reducer(mejorando)

--- src/components/08-useReducer/intro-reducer.js
+++ src/components/08-useReducer/intro-reducer.js
@@ -1,11 +1,10 @@
 // no usar el push en react ya que muta el objeto
-
+console.group(['Estado inicial']);
 const initialState = [{
   id: 1,
   todo: 'Comprar pan',
   done: false
 }];
-
 
 const todoReducer = ( state = initialState, action) => {
 
@@ -17,6 +16,11 @@ const todoReducer = ( state = initialState, action) => {
 }
 
 let todos = todoReducer();
+
+console.table(todos);
+console.groupEnd();
+
+console.group(['Estado despues de la action']);
 
 const newTodo = {
   id: 2,
@@ -30,7 +34,5 @@ const agregarTodoAction = {
 }
 
 todos =  todoReducer( todos , agregarTodoAction )
-
-
-
 console.table(todos)
+console.groupEnd();
