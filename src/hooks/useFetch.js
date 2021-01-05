718b2cd705bff414dbcbdb25a3a084ba60c6abf1
Angel Santillan <angelsb94@gmail.com>
2021-01-04 20:52:00 -0600

    4ta clase test para useFetch

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -4,7 +4,7 @@ export const useFetch = ( url ) => {
 
   const isMounted = useRef(true)
 
-  const [state, setstate] = useState({ data:null, loading: true, error: null})
+  const [state, setState] = useState({ data:null, loading: true, error: null})
 
   useEffect( () => {
     return () => {
@@ -14,12 +14,12 @@ export const useFetch = ( url ) => {
 
   useEffect( () => {
 
-    setstate({ data:null, loading: true, error: null});
+    setState({ data:null, loading: true, error: null});
     fetch( url )
       .then( resp => resp.json() )
       .then( data => {
         if(isMounted.current) {
-          setstate({
+          setState({
             loading: false,
             error: null,
             data
@@ -27,6 +27,9 @@ export const useFetch = ( url ) => {
         } else {
           console.log('setState no se llamo')
         }
+      })
+      .catch( () => {
+        setState({data:null, loading: false, error: 'No se pudo cargar la informacion'})
       })
   }, [url])
 
---
+++ src/test/hooks/useFetch.test.js
@@ -0,0 +1,42 @@
+import { renderHook, act } from "@testing-library/react-hooks"
+import { useFetch } from "../../hooks/useFetch"
+
+
+describe('pruebas en useFetch', () => {
+  const url =  `https://www.breakingbadapi.com/api/quotes/1`
+
+  test('debe de retornar la informacion por defecto', () => {
+
+    const { result } = renderHook( () => useFetch(url) );
+    const { data, loading, error} = result.current;
+    expect( data ).toBe(null);
+    expect( loading ).toBe(true);
+    expect( error ).toBe(null);
+  })
+
+  test('debe de tener la info deseada, loadin: false, error: false', async() => {
+
+    const { result, waitForNextUpdate } = renderHook( () => useFetch(url) );
+    await waitForNextUpdate();
+    const { data, loading, error} = result.current;
+
+    expect( data.length ).toBe(1);
+    expect( loading ).toBe(false);
+    expect( error ).toBe(null);
+    
+  })
+
+  test('debe de manejar el error', async() => {
+    const urlError = 'https://reqres.in/apid/users?page=2'
+    const { result, waitForNextUpdate } = renderHook( () => useFetch(urlError) );
+    await waitForNextUpdate();
+    const { data, loading, error} = result.current;
+
+    expect( data ).toBe(null);
+    expect( loading ).toBe(false);
+    expect( error ).toBe('No se pudo cargar la informacion');
+    
+  })
+
+
+})
